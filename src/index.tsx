88086fda2c0eb13663cb609602bc01ed94f2e3b9
yeajeenchu <yjchu@seegene.com>
2025-02-23 16:31:45 +0900

    chore : add recoil, react-modal  package & add global-styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from './styles/global-styles';
 
-const root = document.getElementById("root") as HTMLElement;
-ReactDOM.createRoot(root).render(<App />);
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(root).render(
+  <RecoilRoot>
+    <GlobalStyle />
+    <App />
+  </RecoilRoot>,
+);
